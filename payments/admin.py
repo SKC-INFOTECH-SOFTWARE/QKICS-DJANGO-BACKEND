1de62d674f4690670001e82043d102f301dd2077
Sandip Dolai <sandip1998dolai@gmail.com>
2025-12-26 12:57:51 +0530

    register Payment model in admin with read-only configuration

--- payments/admin.py
+++ payments/admin.py
@@ -1,3 +1,45 @@
 from django.contrib import admin
+from .models import Payment
 
-# Register your models here.
+
+@admin.register(Payment)
+class PaymentAdmin(admin.ModelAdmin):
+    list_display = (
+        "uuid",
+        "user",
+        "purpose",
+        "amount",
+        "status",
+        "gateway",
+        "created_at",
+    )
+
+    list_filter = (
+        "purpose",
+        "status",
+        "gateway",
+        "created_at",
+    )
+
+    search_fields = (
+        "uuid",
+        "user__email",
+        "reference_id",
+    )
+
+    readonly_fields = (
+        "uuid",
+        "user",
+        "purpose",
+        "reference_id",
+        "amount",
+        "status",
+        "gateway",
+        "gateway_order_id",
+        "gateway_payment_id",
+        "gateway_response",
+        "created_at",
+        "updated_at",
+    )
+
+    ordering = ("-created_at",)
