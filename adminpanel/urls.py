0daf4775ba5ba3200b50cdc7ad118e9afc895e48
Sandip Dolai <sandip1998dolai@gmail.com>
2026-02-18 16:40:57 +0530

    add expert application approval endpoint

--- adminpanel/urls.py
+++ adminpanel/urls.py
@@ -1,6 +1,13 @@
 from django.urls import path
 from .views.users import AdminUserListView
-
+from .views.experts import (
+    AdminExpertApplicationListView,
+    AdminExpertApplicationUpdateView,
+)
 urlpatterns = [
+    # Admin user management
     path("users/", AdminUserListView.as_view(), name="admin-users"),
+    # Admin expert application management
+    path("experts/applications/", AdminExpertApplicationListView.as_view(), name="admin-expert-applications"),
+    path("experts/applications/<int:profile_id>/", AdminExpertApplicationUpdateView.as_view(), name="admin-expert-application-update"),
 ]
