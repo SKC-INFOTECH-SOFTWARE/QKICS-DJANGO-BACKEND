f6da306d72e06c36b2e4fe2d927ce700121a6960
Sandip Dolai <sandip1998dolai@gmail.com>
2026-02-21 13:11:43 +0530

    enhance expert application management

--- adminpanel/views/experts.py
+++ adminpanel/views/experts.py
@@ -10,6 +10,7 @@ from rest_framework.response import Response
 from rest_framework import status
 from django.shortcuts import get_object_or_404
 from django.db import transaction
+from django_filters.rest_framework import DjangoFilterBackend
 
 
 class AdminExpertApplicationListView(ListAPIView):
@@ -22,7 +23,12 @@ class AdminExpertApplicationListView(ListAPIView):
     permission_classes = [IsAuthenticated, IsAdmin]
     pagination_class = AdminPagination
 
-    filter_backends = [SearchFilter, OrderingFilter]
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+
+    filterset_fields = [
+        "application_status",
+        "verified_by_admin",
+    ]
 
     search_fields = [
         "user__username",
@@ -53,41 +59,24 @@ class AdminExpertApplicationUpdateView(APIView):
         action = request.data.get("action")
 
         if action not in ["approve", "reject"]:
-            return Response(
-                {"error": "Invalid action"}, status=status.HTTP_400_BAD_REQUEST
-            )
+            return Response({"error": "Invalid action"}, status=400)
 
-        # Prevent non-superadmin modifying superadmin
         if profile.user.user_type == "superadmin":
             return Response({"error": "Cannot modify superadmin"}, status=403)
 
         if action == "approve":
-
-            if profile.application_status != "pending":
-                return Response(
-                    {"error": "Only pending applications can be approved"}, status=400
-                )
-
-            if profile.user.user_type in ["entrepreneur", "investor"]:
-                return Response(
-                    {"error": "User already has another verified role"}, status=400
-                )
-
             profile.verified_by_admin = True
             profile.application_status = "approved"
             profile.user.user_type = "expert"
-            profile.user.save()
 
         elif action == "reject":
-
-            if profile.application_status != "pending":
-                return Response(
-                    {"error": "Only pending applications can be rejected"}, status=400
-                )
-
             profile.verified_by_admin = False
             profile.application_status = "rejected"
 
+            if profile.user.user_type == "expert":
+                profile.user.user_type = "normal"
+
+        profile.user.save()
         profile.admin_review_note = request.data.get("note", "")
         profile.save()
 
@@ -97,5 +86,5 @@ class AdminExpertApplicationUpdateView(APIView):
                 "profile_id": profile.id,
                 "new_status": profile.application_status,
             },
-            status=200,
+            status=status.HTTP_200_OK,
         )
