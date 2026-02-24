07ad1205c5d9c761d8c75d78dc47b14f81d4c197
Sandip Dolai <sandip1998dolai@gmail.com>
2026-02-24 10:21:51 +0530

    Comment out password validators in settings for flexibility during development

--- rplatform/settings/base.py
+++ rplatform/settings/base.py
@@ -116,12 +116,12 @@ AUTH_USER_MODEL = "users.User"
 # ==================================================
 # PASSWORD VALIDATORS
 # ==================================================
-AUTH_PASSWORD_VALIDATORS = [
-    {"NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator"},
-    {"NAME": "django.contrib.auth.password_validation.MinimumLengthValidator"},
-    {"NAME": "django.contrib.auth.password_validation.CommonPasswordValidator"},
-    {"NAME": "django.contrib.auth.password_validation.NumericPasswordValidator"},
-]
+# AUTH_PASSWORD_VALIDATORS = [
+#     {"NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator"},
+#     {"NAME": "django.contrib.auth.password_validation.MinimumLengthValidator"},
+#     {"NAME": "django.contrib.auth.password_validation.CommonPasswordValidator"},
+#     {"NAME": "django.contrib.auth.password_validation.NumericPasswordValidator"},
+# ]
 
 # ==================================================
 # INTERNATIONALIZATION
