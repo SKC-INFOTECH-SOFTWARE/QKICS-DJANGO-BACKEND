f4e8f0e989e3d1ca45ac85a7ee7ac76f97be1966
Sandip Dolai <sandip1998dolai@gmail.com>
2026-03-19 17:10:31 +0530

    notification register in admin

--- notifications/admin.py
+++ notifications/admin.py
@@ -1,3 +1,41 @@
 from django.contrib import admin
+from .models import Notification
 
-# Register your models here.
+
+@admin.register(Notification)
+class NotificationAdmin(admin.ModelAdmin):
+    list_display = [
+        "user",
+        "event",
+        "title",
+        "status",
+        "is_read",
+        "created_at",
+    ]
+    list_filter = [
+        "status",
+        "is_read",
+        "event",
+        "created_at",
+    ]
+    search_fields = [
+        "user__username",
+        "user__email",
+        "event",
+        "title",
+    ]
+    readonly_fields = [
+        "uuid",
+        "user",
+        "event",
+        "title",
+        "body",
+        "channels",
+        "status",
+        "data",
+        "external_response",
+        "failure_reason",
+        "created_at",
+        "updated_at",
+    ]
+    ordering = ["-created_at"]
